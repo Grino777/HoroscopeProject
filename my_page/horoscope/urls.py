da575d174e907f441ee15b7e38567dee7c89a5a1
Grino777 <bars_B_P@mail.ru>
2023-03-11 12:55:54 +0300

    Added includes folder

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('type', views.get_type_elements),
+    path('', views.index, name='horoscope_index'),
+    path('type', views.get_type_elements, name='elements_of_zodiac'),
     path('type/<str:element_type>', views.get_signs_of_the_zodiac_element, name='signs_of_the_zodiac_element'),
     path('<int:sign_zodiac>', views.get_zodiac_sign_by_number),
     path('<str:sign_zodiac>', views.get_zodiac_sign, name='zodiac_sign'),
